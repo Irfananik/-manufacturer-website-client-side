e6fb8eec74ca034b68619ddac3bf2cbadda73bf6
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-05-21 23:32:50 +0600

    Navbar done with route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Pagas/Components/Navbar';
+import Home from './Pagas/Home';
+import Purchase from './Pagas/Purchase';
+import Login from './Pagas/Register/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>anik</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/purchase" element={<Purchase />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }

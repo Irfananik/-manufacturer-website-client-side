1a3cc8fa3c023f18a37b527af015906759ff5fe0
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-05-28 02:37:16 +0600

    my profile done

--- src/Pagas/Dashboard/MyOrder.js
+++ src/Pagas/Dashboard/MyOrder.js
@@ -31,7 +31,8 @@ const MyOrder = () => {
             .then(data => {
                 setUserEmail(data)
             })
-    }, [user])
+    }, [user, navigate])
+    //console.log(user)
     return (
         <div>
             <h2 className="text-center text-accent my-6">Your Orders Info: {userEmail?.length}</h2>
@@ -40,20 +41,23 @@ const MyOrder = () => {
                     <thead>
                         <tr>
                             <th></th>
-                            <th>Name</th>
                             <th>Email</th>
                             <th>Item</th>
                             <th>Price</th>
+                            <th>Manage</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             userEmail.map((user, index) => <tr>
                                 <th>{index + 1}</th>
-                                <td>{user?.userName}</td>
                                 <td>{user?.email}</td>
                                 <td>{user?.item}</td>
                                 <td>{user?.price}</td>
+                                <td className="me-3">
+                                    <button class="btn btn-sm btn-error">Cancel</button>
+                                    <button class="btn btn-sm">Pay</button>
+                                </td>
                             </tr>)
                         }
                     </tbody>
--- src/Pagas/Dashboard/MyProfile.js
+++ src/Pagas/Dashboard/MyProfile.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
+import { toast } from 'react-toastify';
 
 const MyProfile = () => {
+    const [myProfile, setMyProfile] = useState([])
     const { register, handleSubmit, watch, formState: { errors } } = useForm()
     const [user] = useAuthState(auth)
 
-    const onSubmit = (data, event) => {
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/myprofile?email=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => res.json())
+                .then(data => setMyProfile(data))
+        }
+    }, [user])
 
+    const onSubmit = (data, event) => {
+        const profile = {
+            education: data.education,
+            linkedin: data.linkedin,
+            location: data.location,
+            phone: data.phone,
+            email: user.email
+        }
+        const url = `http://localhost:5000/myprofile/${user?.email}`
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(profile)
+        })
+            .then(res => res.json())
+            .then(posted => {
+                toast('Updated profile')
+            });
+        event.target.reset();
     }
 
     return (
@@ -51,7 +85,7 @@ const MyProfile = () => {
                             </div>
                             <div className="form-control mb-2">
                                 <input
-                                    {...register("linkdin", { required: true })}
+                                    {...register("linkedin", { required: true })}
                                     placeholder="LinkdIn Link"
                                     className="input input-bordered"
                                 />
@@ -63,10 +97,14 @@ const MyProfile = () => {
                         <div class="card lg:max-w-lg bg-base-100 shadow-xl my-6">
                             <div class="card-body">
                                 <h2 class="card-title">Your Profile</h2>
-                                <p>Name:</p>
-                                <p>Education:</p>
-                                <p>Location:</p>
-                                <p>LinkdIn:</p>
+                                {
+                                    myProfile?.map(mp => <div key={mp._id}>
+                                        <h2 className=''>Education: <span className='text-bold '>{mp?.education}</span></h2>
+                                        <h2 className=''>Location: <span className='text-bold '>{mp?.location}</span></h2>
+                                        <h2 className=''>Phone: <span className='text-bold '>{mp?.phone}</span></h2>
+                                        <h2 className=''>Linkedin Id: <span className='text-bold'>{mp?.linkedin}</span></h2>
+                                    </div>)
+                                }
                             </div>
                         </div>
                     </div>
